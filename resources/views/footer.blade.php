d81acbfb4579ab01b37e4572b3bba56ba7038f3e
sherry <sherry@gmail.com>
2023-05-24 19:00:06 +0500

    initial commit

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,23 @@
+<footer>
+  <div class="pull-right">
+    Direct KSA <a href="https://colorlib.com">Colorlib</a>
+  </div>
+  <div class="clearfix"></div>
+</footer>
+<!-- /footer content -->
+</div>
+</div>
+
+<!-- jQuery -->
+<script src="{{ asset('js/jquery.min.js')}}"></script>
+<!-- Bootstrap -->
+<script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
+<!-- FastClick -->
+<script src="'{{asset('js/fastclick.js')}}'"></script>
+<!-- NProgress -->
+<script src="{{asset('js/nprogress.js')}}"></script>
+
+<!-- Custom Theme Scripts -->
+<script src="{{asset('js/custom.min.js')}}"></script>
+</body>
+</html>
---
+++ resources/views/main.blade.php
@@ -0,0 +1,5 @@
+@include('header')
+<div class="container">
+@yield('main-section')
+</div>
+@include('footer')
