b1eda2f3eac5297ead8e20a6c251ea4e6d233b22
aaqib javed <aqib026@gmail.com>
2023-06-11 16:27:49 +0500

    changes for the home page menu

--- resources/views/layouts/front-end.blade.php
+++ resources/views/layouts/front-end.blade.php
@@ -121,88 +121,9 @@
 														</a>
 													</li>
 													<li class="dropdown dropdown-mega">
-														<a class="dropdown-item dropdown-toggle" href="elements.html">
-
+														<a class="dropdown-item" href="elements.html">
 															Elements
-
 														</a>
-														<ul class="dropdown-menu border-top border-top-light mt-0">
-
-															<li>
-																<div class="dropdown-mega-content container px-2">
-																	<div class="row px-1">
-																		<div class="col-lg-3">
-																			<span class="dropdown-mega-sub-title">Elements 1</span>
-																			<ul class="dropdown-mega-sub-nav">
-																				<li><a class="dropdown-item" href="elements-accordions.html">Accordions</a></li>
-																				<li><a class="dropdown-item" href="elements-alerts.html">Alerts</a></li>
-																				<li><a class="dropdown-item" href="elements-animations.html">Animations  <span class="tip tip-dark p-relative bottom-2">hot</span></a></li>
-																				<li><a class="dropdown-item" href="elements-arrows.html">Arrows</a></li>
-																				<li><a class="dropdown-item" href="elements-badges.html">Badges</a></li>
-																				<li><a class="dropdown-item" href="elements-before-after.html">Before / After</a></li>
-																				<li><a class="dropdown-item" href="elements-blockquotes.html">Blockquotes</a></li>
-																				<li><a class="dropdown-item" href="elements-buttons.html">Buttons</a></li>
-																				<li><a class="dropdown-item" href="elements-call-to-action.html">Call to Action</a></li>
-																				<li><a class="dropdown-item" href="elements-cards.html">Cards</a></li>
-																				<li><a class="dropdown-item" href="elements-carousels.html">Carousels</a></li>
-																				<li><a class="dropdown-item" href="elements-cascading-images.html">Cascading Images</a></li>
-																			</ul>
-																		</div>
-																		<div class="col-lg-3">
-																			<span class="dropdown-mega-sub-title">Elements 2</span>
-																			<ul class="dropdown-mega-sub-nav">
-																				<li><a class="dropdown-item" href="elements-content-rotate.html">Content Rotate</a></li>
-																				<li><a class="dropdown-item" href="elements-countdowns.html">Countdowns</a></li>
-																				<li><a class="dropdown-item" href="elements-counters.html">Counters</a></li>
-																				<li><a class="dropdown-item" href="elements-dividers.html">Dividers</a></li>
-																				<li><a class="dropdown-item" href="elements-forms.html">Forms</a></li>
-																				<li><a class="dropdown-item" href="elements-headings.html">Headings</a></li>
-																				<li><a class="dropdown-item" href="elements-icons.html">Icons</a></li>
-																				<li><a class="dropdown-item" href="elements-icon-boxes.html">Icon Boxes</a></li>
-																				<li><a class="dropdown-item" href="elements-image-frames.html">Image Frames  <span class="tip tip-dark p-relative bottom-2">hot</span></a></li>
-																				<li><a class="dropdown-item" href="elements-image-gallery.html">Image Gallery</a></li>
-																				<li><a class="dropdown-item" href="elements-image-hotspots.html">Image Hotspots</a></li>
-																				<li><a class="dropdown-item" href="elements-lightboxes.html">Lightboxes</a></li>
-																			</ul>
-																		</div>
-																		<div class="col-lg-3">
-																			<span class="dropdown-mega-sub-title">Elements 3</span>
-																			<ul class="dropdown-mega-sub-nav">
-																				<li><a class="dropdown-item" href="elements-lists.html">Lists</a></li>						
-																				<li><a class="dropdown-item" href="elements-maps.html">Maps</a></li>
-																				<li><a class="dropdown-item" href="elements-medias.html">Medias</a></li>
-																				<li><a class="dropdown-item" href="elements-modals.html">Modals</a></li>
-																				<li><a class="dropdown-item" href="elements-parallax.html">Parallax</a></li>
-																				<li><a class="dropdown-item" href="elements-particles.html">Particles</a></li>
-																				<li><a class="dropdown-item" href="elements-posts.html">Posts</a></li>
-																				<li><a class="dropdown-item" href="elements-pricing-tables.html">Pricing Tables</a></li>
-																				<li><a class="dropdown-item" href="elements-process.html">Process</a></li>
-																				<li><a class="dropdown-item" href="elements-progressbars.html">Progress Bars</a></li>
-																				<li><a class="dropdown-item" href="elements-random-images.html">Random Images</a></li>
-																				<li><a class="dropdown-item" href="elements-read-more.html">Read More</a></li>
-																			</ul>
-																		</div>
-																		<div class="col-lg-3">
-																			<span class="dropdown-mega-sub-title">Elements 4</span>
-																			<ul class="dropdown-mega-sub-nav">
-																				<li><a class="dropdown-item" href="elements-sections.html">Sections</a></li>
-																				<li><a class="dropdown-item" href="elements-shape-dividers.html">Shape Dividers</a></li>
-																				<li><a class="dropdown-item" href="elements-star-ratings.html">Star Ratings</a></li>
-																				<li><a class="dropdown-item" href="elements-sticky-elements.html">Sticky Elements</a></li>
-																				<li><a class="dropdown-item" href="elements-tables.html">Tables</a></li>
-																				<li><a class="dropdown-item" href="elements-tabs.html">Tabs</a></li>
-																				<li><a class="dropdown-item" href="elements-testimonials.html">Testimonials</a></li>
-																				<li><a class="dropdown-item" href="elements-toggles.html">Toggles</a></li>
-																				<li><a class="dropdown-item" href="elements-tooltips-popovers.html">Tooltips & Popovers</a></li>
-																				<li><a class="dropdown-item" href="elements-typography.html">Typography</a></li>
-																				<li><a class="dropdown-item" href="elements-word-rotator.html">Word Rotator</a></li>
-																				<li><a class="dropdown-item" href="elements-360-image-viewer.html">360º Image Viewer</a></li>
-																			</ul>
-																		</div>
-																	</div>
-																</div>
-															</li>
-														</ul>
 													</li>
 													<li>
 														<a class="nav-link" href="demo-business-consulting-4-about-us.html">
@@ -210,109 +131,16 @@
 														</a>
 													</li>
 													<li class="dropdown">
-														<a class="nav-link dropdown-toggle" href="demo-business-consulting-4-services.html">
+														<a class="nav-link" href="demo-business-consulting-4-services.html">
 															Services
 														</a>
-														<ul class="dropdown-menu">
-															<li>
-																<a href="demo-business-consulting-4-services-detail.html" class="dropdown-item">Strategic Planning</a>
-															</li>
-															<li>
-																<a href="demo-business-consulting-4-services-detail.html" class="dropdown-item">Financial Clean-Up</a>
-															</li>
-															<li>
-																<a href="demo-business-consulting-4-services-detail.html" class="dropdown-item">Cash Flow Planning</a>
-															</li>
-														</ul>
 													</li>
 													<li class="dropdown">
-														<a class="dropdown-item dropdown-toggle" href="#">
+														<a class="dropdown-item" href="#">
 
 															Portfolio
 
 														</a>
-														<ul class="dropdown-menu">
-
-															<li class="dropdown-submenu">
-																<a class="dropdown-item" href="#">Single Project</a>
-																<ul class="dropdown-menu">
-
-																	<li><a class="dropdown-item" href="portfolio-single-wide-slider.html">Wide Slider</a></li>
-																	<li><a class="dropdown-item" href="portfolio-single-small-slider.html">Small Slider</a></li>
-																	<li><a class="dropdown-item" href="portfolio-single-full-width-slider.html">Full Width Slider</a></li>
-																	<li><a class="dropdown-item" href="portfolio-single-gallery.html">Gallery</a></li>
-																	<li><a class="dropdown-item" href="portfolio-single-carousel.html">Carousel</a></li>
-																	<li><a class="dropdown-item" href="portfolio-single-medias.html">Medias</a></li>
-																	<li><a class="dropdown-item" href="portfolio-single-full-width-video.html">Full Width Video</a></li>
-																	<li><a class="dropdown-item" href="portfolio-single-masonry-images.html">Masonry Images</a></li>
-																	<li><a class="dropdown-item" href="portfolio-single-left-sidebar.html">Left Sidebar</a></li>
-																	<li><a class="dropdown-item" href="portfolio-single-right-sidebar.html">Right Sidebar</a></li>
-																	<li><a class="dropdown-item" href="portfolio-single-left-and-right-sidebars.html">Left and Right Sidebars</a></li>
-																	<li><a class="dropdown-item" href="portfolio-single-sticky-sidebar.html">Sticky Sidebar</a></li>
-																	<li><a class="dropdown-item" href="portfolio-single-extended.html">Extended</a></li>
-																</ul>
-															</li>
-															<li class="dropdown-submenu">
-																<a class="dropdown-item" href="#">Grid Layouts</a>
-																<ul class="dropdown-menu">
-
-																	<li><a class="dropdown-item" href="portfolio-grid-1-column.html">1 Column</a></li>
-																	<li><a class="dropdown-item" href="portfolio-grid-2-columns.html">2 Columns</a></li>
-																	<li><a class="dropdown-item" href="portfolio-grid-3-columns.html">3 Columns</a></li>
-																	<li><a class="dropdown-item" href="portfolio-grid-4-columns.html">4 Columns</a></li>
-																	<li><a class="dropdown-item" href="portfolio-grid-5-columns.html">5 Columns</a></li>
-																	<li><a class="dropdown-item" href="portfolio-grid-6-columns.html">6 Columns</a></li>
-																	<li><a class="dropdown-item" href="portfolio-grid-no-margins.html">No Margins</a></li>
-																	<li><a class="dropdown-item" href="portfolio-grid-full-width.html">Full Width</a></li>
-																	<li><a class="dropdown-item" href="portfolio-grid-full-width-no-margins.html">Full Width No Margins</a></li>
-																	<li><a class="dropdown-item" href="portfolio-grid-1-column-title-and-description.html">Title and Description</a></li>
-																</ul>
-															</li>
-															<li class="dropdown-submenu">
-																<a class="dropdown-item" href="#">Masonry Layouts</a>
-																<ul class="dropdown-menu">
-
-																	<li><a class="dropdown-item" href="portfolio-masonry-2-columns.html">2 Columns</a></li>
-																	<li><a class="dropdown-item" href="portfolio-masonry-3-columns.html">3 Columns</a></li>
-																	<li><a class="dropdown-item" href="portfolio-masonry-4-columns.html">4 Columns</a></li>
-																	<li><a class="dropdown-item" href="portfolio-masonry-5-columns.html">5 Columns</a></li>
-																	<li><a class="dropdown-item" href="portfolio-masonry-6-columns.html">6 Columns</a></li>
-																	<li><a class="dropdown-item" href="portfolio-masonry-no-margins.html">No Margins</a></li>
-																	<li><a class="dropdown-item" href="portfolio-masonry-full-width.html">Full Width</a></li>
-																</ul>
-															</li>
-															<li class="dropdown-submenu">
-																<a class="dropdown-item" href="#">Sidebar Layouts</a>
-																<ul class="dropdown-menu">
-
-																	<li><a class="dropdown-item" href="portfolio-sidebar-left.html">Left Sidebar</a></li>
-																	<li><a class="dropdown-item" href="portfolio-sidebar-right.html">Right Sidebar</a></li>
-																	<li><a class="dropdown-item" href="portfolio-sidebar-left-and-right.html">Left and Right Sidebars</a></li>
-																	<li><a class="dropdown-item" href="portfolio-sidebar-sticky.html">Sticky Sidebar</a></li>
-																</ul>
-															</li>
-															<li class="dropdown-submenu">
-																<a class="dropdown-item" href="#">Ajax</a>
-																<ul class="dropdown-menu">
-
-																	<li><a class="dropdown-item" href="portfolio-ajax-page.html">Ajax on Page</a></li>
-																	<li><a class="dropdown-item" href="portfolio-ajax-modal.html">Ajax on Modal</a></li>
-																</ul>
-															</li>
-															<li class="dropdown-submenu">
-																<a class="dropdown-item" href="#">Extra</a>
-																<ul class="dropdown-menu">
-
-																	<li><a class="dropdown-item" href="portfolio-extra-timeline.html">Timeline</a></li>
-																	<li><a class="dropdown-item" href="portfolio-extra-lightbox.html">Lightbox</a></li>
-																	<li><a class="dropdown-item" href="portfolio-extra-load-more.html">Load More</a></li>
-																	<li><a class="dropdown-item" href="portfolio-extra-infinite-scroll.html">Infinite Scroll</a></li>
-																	<li><a class="dropdown-item" href="portfolio-extra-lazy-load-masonry.html">Lazy Load Masonry</a></li>
-																	<li><a class="dropdown-item" href="portfolio-extra-pagination.html">Pagination</a></li>
-																	<li><a class="dropdown-item" href="portfolio-extra-combination-filters.html">Combination Filters</a></li>
-																</ul>
-															</li>
-														</ul>
 													</li>
 													<li>
 														<a class="nav-link" href="demo-business-consulting-4-team.html">
