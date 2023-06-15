a14e4c132c05494d210ace50db5bdb44f9145f95
aqibtosol <aqibtosol@gmail.com>
2023-06-15 15:53:25 +0500

    design fixes

--- resources/views/auth/editregister.blade.php
+++ resources/views/auth/editregister.blade.php
@@ -1,92 +1,96 @@
 @extends('...admin.include.main')
 
 @section('main-section')
-	@push('title')
-		<title>Add-User</title>
-	@endpush
-	<div class="page-title">
-		<div class="title_left">
-			<h3>{{$title}}</h3>
-		</div>
-		<div class="title_right">
-			<div class="col-md-1 col-sm-6  form-group pull-right top_search">
-				<a href="{{url('admin/users')}}"><button class="btn btn-danger">Back</button></a>
+@push('title')
+<title>Add-User</title>
+@endpush
+<div class="row">
+	<div class="col-lg-12">
+		<div class="page-title">
+			<div class="title_left">
+				<h3>{{$title}}</h3>
+			</div>
+			<div class="title_right">
+				<div class="col-md-1 col-sm-6  form-group pull-right top_search">
+					<a href="{{url('admin/users')}}"><button class="btn btn-danger">Back</button></a>
+				</div>
 			</div>
 		</div>
-	</div>
-	<div class="clearfix"></div>
-	<div class="row">
-		<div class="col-md-12 col-sm-12">
-			<div class="x_panel">
-				<div class="x_title">
-					<h2>{{$title}}</h2>
-					<div class="clearfix"></div>
-				</div>
-				<div class="x_content">
-					<br />
-					<form method="POST" action="{{$url}}">
-						@csrf
-						<div class="item form-group">
-							<label class="col-form-label col-md-3 col-sm-3 label-align" for="first-name">Name <span class="required">*</span>
-							</label>
-							<div class="col-md-6 col-sm-6 ">
-								<input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{old('name',isset($user)?$user->name:'')}}" required autocomplete="name" placeholder="Enter Your Name" autofocus>
-								@error('name')
-								<span class="invalid-feedback" role="alert">
-									<strong>{{ $message }}</strong>
-								</span>
-								@enderror
+		<div class="clearfix"></div>
+		<div class="row">
+			<div class="col-md-12 col-sm-12">
+				<div class="x_panel">
+					<div class="x_title">
+						<h2>{{$title}}</h2>
+						<div class="clearfix"></div>
+					</div>
+					<div class="x_content">
+						<br />
+						<form method="POST" action="{{$url}}">
+							@csrf
+							<div class="item form-group">
+								<label class="col-form-label col-md-3 col-sm-3 label-align" for="first-name">Name <span class="required">*</span>
+								</label>
+								<div class="col-md-6 col-sm-6 ">
+									<input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{old('name',isset($user)?$user->name:'')}}" required autocomplete="name" placeholder="Enter Your Name" autofocus>
+									@error('name')
+									<span class="invalid-feedback" role="alert">
+										<strong>{{ $message }}</strong>
+									</span>
+									@enderror
+								</div>
 							</div>
-						</div>
-						<div class="item form-group">
-							<label class="col-form-label col-md-3 col-sm-3 label-align" for="last-name">Email <span class="required">*</span>
-							</label>
-							<div class="col-md-6 col-sm-6 ">
-								<input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{old('name',isset($user)?$user->email:'')}}" placeholder="Enter Your Email" required autocomplete="email">
-								@error('email')
-								<span class="invalid-feedback" role="alert">
-									<strong>{{ $message }}</strong>
-								</span>
-								@enderror
+							<div class="item form-group">
+								<label class="col-form-label col-md-3 col-sm-3 label-align" for="last-name">Email <span class="required">*</span>
+								</label>
+								<div class="col-md-6 col-sm-6 ">
+									<input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{old('name',isset($user)?$user->email:'')}}" placeholder="Enter Your Email" required autocomplete="email">
+									@error('email')
+									<span class="invalid-feedback" role="alert">
+										<strong>{{ $message }}</strong>
+									</span>
+									@enderror
+								</div>
 							</div>
-						</div>
-						<div class="item form-group">
-							<label for="middle-name" class="col-form-label col-md-3 col-sm-3 label-align">Password</label>
-							<div class="col-md-6 col-sm-6 ">
-								<input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="Enter Password" autocomplete="new-password">
+							<div class="item form-group">
+								<label for="middle-name" class="col-form-label col-md-3 col-sm-3 label-align">Password</label>
+								<div class="col-md-6 col-sm-6 ">
+									<input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="Enter Password" autocomplete="new-password">
 
-								@error('password')
-								<span class="invalid-feedback" role="alert">
-									<strong>{{ $message }}</strong>
-								</span>
-								@enderror
+									@error('password')
+									<span class="invalid-feedback" role="alert">
+										<strong>{{ $message }}</strong>
+									</span>
+									@enderror
+								</div>
 							</div>
-						</div>
-						<div class="item form-group">
-							<label for="middle-name" class="col-form-label col-md-3 col-sm-3 label-align">Confirm Password</label>
-							<div class="col-md-6 col-sm-6 ">
-								<input id="password-confirm" type="password" class="form-control" name="password_confirmation" placeholder="Confirm Password" autocomplete="new-password">
+							<div class="item form-group">
+								<label for="middle-name" class="col-form-label col-md-3 col-sm-3 label-align">Confirm Password</label>
+								<div class="col-md-6 col-sm-6 ">
+									<input id="password-confirm" type="password" class="form-control" name="password_confirmation" placeholder="Confirm Password" autocomplete="new-password">
+								</div>
 							</div>
-						</div>
-						<div class="item form-group">
-							<label class="col-form-label col-md-3 col-sm-3 label-align">Gender</label>
-							<div class="col-md-6 col-sm-6 ">
-								<select class="form-control" name="usertype" id="usertype">
-									<option value="users">User</option>
-									<option value="admin">Admin</option>
-								</select>
+							<div class="item form-group">
+								<label class="col-form-label col-md-3 col-sm-3 label-align">Gender</label>
+								<div class="col-md-6 col-sm-6 ">
+									<select class="form-control" name="usertype" id="usertype">
+										<option value="users">User</option>
+										<option value="admin">Admin</option>
+									</select>
+								</div>
 							</div>
-						</div>
-						<div class="ln_solid"></div>
-						<div class="item form-group">
-							<div class="col-md-6 col-sm-6 offset-md-3">
-								<a href="{{url('../admin/users')}}"> <button class="btn btn-primary" type="button">Cancel</button></a>
-								<button type="submit" class="btn btn-success">Submit</button>
+							<div class="ln_solid"></div>
+							<div class="item form-group">
+								<div class="col-md-6 col-sm-6 offset-md-3">
+									<a href="{{url('../admin/users')}}"> <button class="btn btn-primary" type="button">Cancel</button></a>
+									<button type="submit" class="btn btn-success">Submit</button>
+								</div>
 							</div>
-						</div>
-					</form>
+						</form>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
+</div>
 @endsection
