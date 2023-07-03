7d705f90475dc68649b065dd416b13e610f1faa6
shari <sharisaleem0@gmail.com>
2023-07-03 18:43:20 +0500

    s

--- resources/views/user/layout/dashboard.blade.php
+++ resources/views/user/layout/dashboard.blade.php
@@ -8,7 +8,7 @@
     <!-- Basic -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+@stack('title')
     <title>Direct-KSA</title>
 
     <meta name="keywords" content="Visa" />
