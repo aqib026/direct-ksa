6213af5e2b1ee3edf40112c9371e16c313cc4172
aaqib javed <aqib026@gmail.com>
2023-08-01 15:31:18 +0500

    changes for he hel

--- app/Http/Resources/RequirementResource.php
+++ app/Http/Resources/RequirementResource.php
@@ -19,22 +19,39 @@ class RequirementResource extends JsonResource
             'countries_id' => $this->countries_id,
             'country' => $this->visa->name, // Assuming the country name is stored in the `name` column
             'status' => $this->status,
-//<<<<<<< HEAD
-            'mobile_detail'=>strip_tags($this->mobile_detail),
-            'mobile_detail_ar'=>strip_tags($this->mobile_detail_ar),
-//=======
-//            'mobile_detail'=> $this->format_detail($this->mobile_detail),
-//            'mobile_detail_ar'=>  $this->format_detail($this->mobile_detail_ar),
-//>>>>>>> 8d4d0751454fd23cce990d7b295696500cc80be1
+            'mobile_detail'=>$this->format_detail($this->mobile_detail),
+            'mobile_detail_ar'=>$this->format_detail($this->mobile_detail_ar),
      
             ];
     }
 
-//    protected function format_detail($mobile_detail){
-//        $mobile_detail =  strip_tags($mobile_detail);
-//        $mobile_detail = explode(PHP_EOL, $mobile_detail);
-//        $mobile_detail = json_encode($mobile_detail);
-//        return $mobile_detail;
-//    }
-//
+   protected function format_detail($mobile_detail){
+
+    $dom = new \DOMDocument();
+
+    $dom->loadHTML($mobile_detail);
+    // Initialize an array to store the extracted data
+    $extractedData = array();
+
+    // Function to recursively traverse the DOM tree and build the array
+    function extractDataFromNode($node, &$data) {
+        if ($node->nodeType === XML_TEXT_NODE) {
+            // If it's a text node, add the text content to the array
+            $data[] = $node->textContent;
+        } else {
+            // If it's an element node, process its children
+            foreach ($node->childNodes as $childNode) {
+                extractDataFromNode($childNode, $data);
+            }
+        }
+    }
+
+    // Start extracting data from the root element (in this case, <div>)
+    extractDataFromNode($dom->documentElement, $extractedData);
+
+    print_r($extractedData);    
+   }
+
+
+
 }
