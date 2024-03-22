2dc816f41e5df445c8248c5015bf14e58e813b80
OsamaTAbbasi <osama.taimoor.abbasi@gmail.com>
2024-03-22 16:56:51 +0500

    Login and Register to Arabic lang

---
+++ lang/ar/Register.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'create_account' => 'إنشاء حساب',
+    'name' => 'الاسم',
+    'enter_your_name' => 'أدخل اسمك',
+    'email_address' => 'عنوان البريد الإلكتروني',
+    'enter_your_email' => 'أدخل بريدك الإلكتروني',
+    'password' => 'كلمة المرور',
+    'enter_password' => 'أدخل كلمة المرور',
+    'confirm_password' => 'تأكيد كلمة المرور',
+    'phone_number' => 'رقم الهاتف',
+    'register' => 'تسجيل',
+    'already_member' => 'هل أنت عضو بالفعل؟',
+    'log_in' => 'تسجيل الدخول',
+];
---
+++ lang/en/login.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'sign_in_with' => 'Sign in with',
+    'mobile_no' => 'Mobile No',
+    'email' => 'Email',
+    'enter_your_email' => 'Enter Your Email',
+    'password' => 'Password',
+    'enter_your_password' => 'Enter Your Password',
+    'remember_me' => 'Remember me',
+    'forgot_password' => 'Forgot password?',
+    'login_with_otp' => 'Login With OTP',
+    'login' => 'Login',
+    'sign_in_with_password' => 'Sign In With Password',
+    'dont_have_account' => "Don't have an account?",
+    'register' => 'Register',
+];
