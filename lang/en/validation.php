baccd74084eb72fd56346532029f0b7b0ea067f5
aaqib javed <aqib026@gmail.com>
2024-03-25 21:40:07 +0500

    chages for the shelo

--- lang/en/validation.php
+++ lang/en/validation.php
@@ -144,7 +144,7 @@ return [
     'starts_with' => 'The :attribute must start with one of the following: :values.',
     'string' => 'The :attribute must be a string.',
     'timezone' => 'The :attribute must be a valid timezone.',
-    'unique' => 'The :attribute has already been taken.',
+    'unique' => 'The :attribute is already registered. Please Login using the using OTP or recover account using forgot password.',
     'uploaded' => 'The :attribute failed to upload.',
     'uppercase' => 'The :attribute must be uppercase.',
     'url' => 'The :attribute must be a valid URL.',
--- resources/lang/en/validation.php
+++
@@ -1,5 +0,0 @@
-<?php 
-return [
-    'unique' => 'The :attribute is already registered. Please Login using the using OTP or recover account using forgot password.',
-]
-?>
